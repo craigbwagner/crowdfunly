66d5753ad24de29e6b80a0b76770199c91ef9caa
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-09 15:22:17 -0400

    move test-jwt routes and controllers to separate files and folders

--- controllers/test-jwt.js
+++ controllers/test-jwt.js
@@ -1,18 +1,6 @@
-const express = require('express');
-const router = express.Router();
 const jwt = require('jsonwebtoken');
 
-router.get('/sign-token', (req, res) => {
-  const user = {
-    _id: 1,
-    username: 'Jeff',
-    password: 'test'
-  }
-  const token = jwt.sign({user}, process.env.JWT_SECRET);
-  res.json({token})
-});
-
-router.post('/verify-token', (req, res) => {
+async function verifyToken(req, res) {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -20,6 +8,16 @@ router.post('/verify-token', (req, res) => {
   } catch (error) {
     res.status(401).json({ error: 'Invalid token'});
   }
-})
+}
 
-module.exports = router;
+async function signToken(req, res) {
+  const user = {
+    _id: 1,
+    username: 'Jeff',
+    password: 'test'
+  }
+  const token = jwt.sign({user}, process.env.JWT_SECRET);
+  res.json({token})
+}
+
+module.exports = {verifyToken, signToken}
