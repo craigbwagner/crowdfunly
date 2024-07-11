fe6d92a7b921c86d11d27357c2341fd7dde1b7ac
jcao91 <jcao91@yahoo.com>
2024-07-11 14:49:23 -0500

    deleteCampaign function try catch block setup

--- controllers/campaigns.js
+++ controllers/campaigns.js
@@ -56,4 +56,11 @@ const update = async (req, res) => {
   }
 };
 
-module.exports = { create, index, show, update };
+const deleteCampaign = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { create, index, show, update, deleteCampaign };
