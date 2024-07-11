16d431082a38bb37a143d32e8c62687b9e6461a8
jcao91 <jcao91@yahoo.com>
2024-07-11 12:11:33 -0500

    routes and controllers folder created and connected together and in server.js

---
+++ controllers/campaigns.js
@@ -0,0 +1,1 @@
+const Campaign = require ('../models/campaign');
--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require(('cors'));
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const testJWTRouter = require('./routes/test-jwt');
-const usersRouter = require('./routes/users');
-const profilesRouter = require('./routes/profiles');
+const testJWTRouter = require("./routes/test-jwt");
+const usersRouter = require("./routes/users");
+const profilesRouter = require("./routes/profiles");
+const campaignsController = require("./routes/campaigns.js");
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
+mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/test-jwt', testJWTRouter);
-app.use('/users', usersRouter);
-app.use('/profiles', profilesRouter);
+app.use("/test-jwt", testJWTRouter);
+app.use("/users", usersRouter);
+app.use("/profiles", profilesRouter);
+app.use("/campaigns", campaignsController);
 
 app.listen(3000, () => {
-  console.log('The express app is ready!');
+  console.log("The express app is ready!");
 });
