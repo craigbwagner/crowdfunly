203cbb0e36e24ef95844f289a7d93f0147e97c0c
matthew-estes <matthew.estes19@gmail.com>
2024-07-11 11:31:54 -0400

    fetch all campaigns from database

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get ('/', async (req, res) => {
     try {
-        
+        const campaigns = await Campaign.find()
     } catch (error) {
         
     }
