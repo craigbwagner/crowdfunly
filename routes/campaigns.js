dd343209ba435b88ddb33c2de47cbaa3debf9e2a
matthew-estes <matthew.estes19@gmail.com>
2024-07-11 11:40:50 -0400

    send fetched campaigns with 201 status

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const verifyToken = require("../middleware/verify-token.js");
 const Campaign = ('../models/campaign');
 const router = express.Router();
 
 router.get ('/', async (req, res) => {
     try {
         const campaigns = await Campaign.find()
-        .populate('createdBy', 'username')
-        populate('contributions')
+        .populate('createdBy', 'username');
+        populate('contributions');
+        res.status(201).json(compaigns);
     } catch (error) {
+       
         
     }
 
