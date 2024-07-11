fc692e4241211516dd5d6effe8d2f969ca884dcd
jcao91 <jcao91@yahoo.com>
2024-07-11 13:00:19 -0500

    add conditional for creating campaign

--- controllers/campaigns.js
+++ controllers/campaigns.js
@@ -2,7 +2,9 @@ const Campaign = require("../models/campaign");
 
 const create = async (req, res) => {
   try {
+    req.body.createdBy = req.user._id;
     const createdCampaign = await Campaign.create(req.body);
+    createdCampaign._doc.createdBy = req.user;
     res.status(201).json(createdCampaign);
   } catch (error) {
     console.log(error);
