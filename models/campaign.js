81200aae554d6cd3977874bd25193ef2754a66cd
jcao91 <jcao91@yahoo.com>
2024-07-11 12:00:39 -0500

    create const Campaign on models to export

--- models/campaign.js
+++ models/campaign.js
@@ -1,39 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const campaignSchema = new mongoose.Schema({
+const campaignSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     createdBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     goalAmount: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     amountRaised: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
-    contributions: [{
+    contributions: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Contribution'
-    }],
+        ref: "Contribution",
+      },
+    ],
     endDate: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     campaignType: {
-        type: String,
-        enum: ['type1', 'type2', 'type3','type4'] //create and replace with actual campaign types!
-    }
-}, {timestamps: true});
+      type: String,
+      enum: ["type1", "type2", "type3", "type4"], //create and replace with actual campaign types!
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Campaign', campaignSchema);
+const Campaign = mongoose.model("Campaign", campaignSchema);
+module.exports = Campaign;
