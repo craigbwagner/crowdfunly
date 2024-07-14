414d72137a82e4a1429e2020a79e3ded42bf777b
matthew-estes <matthew.estes19@gmail.com>
2024-07-14 10:33:55 -0400

    create payment intent

--- routes/stripe.js
+++ routes/stripe.js
@@ -2,3 +2,24 @@ const express = require('express');
 const router = express.Router();
 const Stripe = require('stripe');
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+router.post('/create-payment-intent', async (req, res) =>{
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: 500,
+            currency: 'usd',
+            payment_method: 'pm_card_visa',
+          }); 
+    } catch (
+        
+
+
+    ) {
+        
+    }
+
+
+
+
+
+})
