2775bd2d957a14afc9fa398a6399251baed45f4e
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-16 15:22:15 -0400

    add verifyToken middleware to contributions router

--- routes/contributions.js
+++ routes/contributions.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const contributionsController = require('../controllers/contributions')
+const contributionsController = require('../controllers/contributions');
+const verifyToken = require("../middleware/verify-token");
+
+router.use(verifyToken);
 
 router.get("/", contributionsController.index);
-
 router.post("/:campaignId", contributionsController.createContribution);
 
 module.exports = router;
